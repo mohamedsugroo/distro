421e93e5e4218662f05bfcffceb63ed086e7b70f
mohamedsugroo <mohamedsugroo@gmail.com>
2019-01-12 16:43:23 +0000

    first commit

---
+++ js/about.539cc226.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{"619a":function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],u=n("2877"),i={},o=Object(u["a"])(i,a,s,!1,null,null,null);o.options.__file="whatwedo.vue";e["default"]=o.exports}}]);
+//# sourceMappingURL=about.539cc226.js.map
---
+++ precache-manifest.2979faaab493b97eab1d9a6af5b94362.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "31a1ae5851387f74cc9b",
+    "url": "/js/chunk-vendors.b7efa852.js"
+  },
+  {
+    "revision": "4473b4a845647e786bd3",
+    "url": "/js/app.eeafa573.js"
+  },
+  {
+    "revision": "b691942de1f99fd21675",
+    "url": "/js/about.539cc226.js"
+  },
+  {
+    "revision": "d3a8e6993369656dfac77f496faf5abf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4473b4a845647e786bd3",
+    "url": "/css/app.6d0fe1e8.css"
+  }
+];
